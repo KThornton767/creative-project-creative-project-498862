7017c36e153c9271d0e15166cd1499a6c56318de
Kaitlyn <kthornton767@gmail.com>
2025-04-22 18:53:10 -0500

    About page and login page updated

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './CSS/About.css';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='biobox'>
+      <img src="../Components/Assests/logo.png" alt="" className="profilepic" />
+      <p className="about-seller">
+        Hi! My name is Kaitlyn, and I've been skating since I was young. This is a store where you can get what you need when it comes to skating equipment!
+      </p>
+    </div>
   )
 }
 
--- src/Pages/LoginRegister.jsx
+++ src/Pages/LoginRegister.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './CSS/LoginRegister.css'
+
 
 const LoginRegister = () => {
   return (
-    <div>LoginRegister</div>
+    <div class="loginregister">
+      <div class="loginbox">
+        <h1>Sign Up</h1>
+        <div className="loginfields">
+          <input type="text" placeholder='Your Name'></input>
+          <input type="text" placeholder='Email Address'></input>
+          <input type="text" placeholder='Password'></input>
+        </div>
+        <button>Continue</button>
+        <p className='loginswitch'>Already signed up? <span>Login here!</span>  </p> 
+      </div>
+    </div>
   )
 }
 
