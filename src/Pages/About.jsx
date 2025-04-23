895a3be78d94f9f2194216664bf40cda7492671f
Kaitlyn <kthornton767@gmail.com>
2025-04-22 21:05:35 -0500

    more changes to about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './CSS/About.css';
 
+import logo from '../Components/Assests/logo.png'
+
 const About = () => {
   return (
     <div className='biobox'>
-      <img src="../Components/Assests/logo.png" alt="" className="profilepic" />
+      <img src={logo} alt="" className="profilepic" />
+      <div className="textbox">
       <p className="about-seller">
         Hi! My name is Kaitlyn, and I've been skating since I was young. This is a store where you can get what you need when it comes to skating equipment!
       </p>
+      </div>
+
     </div>
   )
 }
