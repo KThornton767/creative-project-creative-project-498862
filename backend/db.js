fbfc6b98d262fce868ec9e2a7347a8f3fea119a6
Kaitlyn <kthornton767@gmail.com>
2025-04-23 02:11:42 -0500

    trying to integrate backend to pull items

--- backend/db.js
+++ backend/db.js
@@ -1,35 +1,81 @@
 require("dotenv").config();
 const mysql = require("mysql");
+const saltRounds = 10;
 
 const host = process.env.MYSQL_HOST;
 const database = process.env.MYSQL_DB;
 
 console.log(`connecting to database ${database} on ${host}`);
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.MYSQL,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
 });
 
-// Make the connection
-connection.connect(function (err) {
-  // Check if there is a connection error
-  if (err) {
-      console.log("connection error", err.stack);
-      return;
-  }
+function getAllItems(limit = 100) {
+  return new Promise((resolve, reject) => {
+      const sql = `SELECT * FROM items LIMIT ?`;
+      pool.query(sql, [limit], function (err, results, fields) {
+          if (err) {
+              return reject(err);
+          }
 
-  // If there was no error, print this message
-  console.log(`connected to database`);
-});
+          return resolve(results);
+      });
+  });
+}
 
-//const sql = "SELECT * FROM users";
-//connection.query(sql, function (err, results, fields) {
-//    if (err) throw err;
+function getCartById(id) {
+  return new Promise((resolve, reject) => {
+      const sql = "SELECT * FROM carts WHERE id = ?";
+      pool.query(sql, [id], function (err, results, fields) {
+          if (err) {
+              return reject(err);
+          }
 
-//    console.log("here are your results", results);
-//});
+          return resolve(results);
+      });
+  });
+}
 
-// connection.end();
+function getUserByUsername(username) {
+  return new Promise((resolve, reject) => {
+      // This is an alternate way to sanitize user input for the query
+      const sql =
+          "SELECT * FROM users WHERE username= ? " +
+          pool.escape(`%${username}%`);
+
+      pool.query(sql, [username], function (err, results, fields) {
+          if (err) {
+              return reject(err);
+          }
+
+          return resolve(results);
+      });
+  });
+}
+
+function addUser(username,email,passhash){
+  return new Promise((resolve,reject)=> {
+      const sql =
+      "INSERT INTO users (id, username, email, passhash) values (NULL, ?, ?, ?)"
+      pool.query(sql,[username,email,passhash]), function(err,fields){
+        if (err){
+          return reject(err);
+        }
+        return resolve;
+      }
+  });
+
+}
+
+
+module.exports = {
+  getAllItems,
+  getUserByUsername,
+  getCartById,
+  addUser
+};
--- src/Pages/ShopPage.jsx
+++ src/Pages/ShopPage.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Item from '../Components/Item/Item.jsx';
+const db = require("../../backend/db.js");
+
 
 const ShopPage = () => {
+
+  db.getAllItems(10)
+    .then(data => res.json(data)) // if successful
+    .catch(err => res.status(500).json(err)); // if error
+
   return (
-    <div>ShopPage</div>
+    <div className="all-items">
+      
+    </div>
   )
 }
 
